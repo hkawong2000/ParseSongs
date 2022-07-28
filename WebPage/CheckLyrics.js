ec3471a12f402e7d5dbf7f2ab0acd726106c4163
hkawong2000 <hkawong2000@gmail.com>
2022-07-28 09:58:09 +0800

    Add code to save result page (as blob)

--- WebPage/CheckLyrics.js
+++ WebPage/CheckLyrics.js
@@ -1,27 +1,37 @@
 // Global Variables
 
-var inputFileSet = false;
-var inputFile    = null;
+var inputFileSet   = false;
+var inputFile      = null;
+var outputFileName = null;
 //
 const resultButton = document.getElementById("viewResult");
-//
-var resultContent = '';
-var resultHeader1 = '<html>\n\n<head>\n\n<meta charset="utf-8">\n';
-var resultHeader2 = '<title> Result </title>\n';
-var resultHeader3 = '<link type="text/css" rel="stylesheet" href="./CheckLyrics.css">\n';
-var resultHeader4 = '</head>\n';
-var resultHeader5 = '<body>\n\n<h1> Result </h1>\n';
-var resultFooter  = '\n</body>\n\n</html>\n\n';
 
+// Result texts
+// - header
+var resHdr1 = '<html>\n\n<head>\n\n<meta charset="utf-8">\n\n';                         // head-begin and charset
+var resHdr2 = '<title> Result (to be changed !!!!) </title>\n\n';                       // page title
+var resHdr3 = '<link type="text/css" rel="stylesheet" href="./CheckLyrics.css">\n\n';   // style sheet
+var resHdr4 = '</head>\n\n';                                                            // head-end
+// - body
+var resBody1 = '<body>\n\n'                                                             // body-begin
+var resBody2 = '<h1> Result (to be changed !!!!) </h1>\n';                              // body-header                                                   
+var resBody  = '';
+// - footer
+var resFooter = '\n</body>\n\n</html>\n\n';
+
+// For storing contents of lines
 var LineContentList = new Array(200);
 
 
 function SaveFilePath(input)
 {
-    inputFile = input.files[0];
-    inputFileSet = true;
+    inputFile      = input.files[0];
+    inputFileSet   = true;
+    inputFileName  = inputFile.name;
+    outputFileName = inputFileName.replace('.txt', '_out.html');
+    console.log('outputFileName = ' + outputFileName);
 }
-
+ 
 
 function ProcessInputLine(idx, curLine)
 {
@@ -60,29 +70,55 @@ function ProcessInputLine(idx, curLine)
                 LineContentList[idx] = outContent;
             }
         }
-        resultContent += ('<p class="testResult"> Line' + idx + ' : ' + outContent + '\n');
+        resBody += ('<p class="testResult"> Line' + idx + ' : ' + outContent + '\n');
     }
 }
 
 
 function WriteResult()
 {
+    var     outputText;
+    var     resHdrAll, resBodyAll;
+    
     console.log('WriteResult() called');
     resultWin = window.open();
-    resultWin.document.write(resultHeader1);
-    resultWin.document.write(resultHeader2);
-    resultWin.document.write(resultHeader3);
-    resultWin.document.write(resultHeader4);
-    resultWin.document.write(resultHeader5);
-    resultWin.document.write(resultContent);
-    resultWin.document.write(resultFooter);
-    console.log('WriteResult() end');
+    
+    // Result page header
+    resHdrAll = resHdr1 + resHdr2 + resHdr3 + resHdr4;
+    resultWin.document.write(resHdrAll);
+    // Result page body
+    resBodyAll = resBody1 + resBody2 + resBody;
+    resultWin.document.write(resBodyAll);
+    
+    outputText = resHdrAll + resBodyAll + resFooter;
+
+    // Save result in file
+    // ref: https://stackoverflow.com/questions/32326973/file-write-operation-in-javascript
+    var filename   = outputFileName;
+    var text       = outputText;
+    var blob       = new Blob([text], {type:'text/plain'});
+    var link       = document.createElement("a");
+    link.download  = filename;
+    link.innerHTML = "Download File";
+    link.href = window.URL.createObjectURL(blob);
+    //x document.body.appendChild(link);
+    
+    // Blob for download
+    saveStr = '<a download="' + outputFileName + '" href="' + link.toString() + '">Download File</a>';
+    resultWin.document.write('<hr>');
+    resultWin.document.write(saveStr);
+
+    // Result page footer
+    resultWin.document.write(resFooter);
+
     resultButton.disabled = true;
 }
 
 
 function Process_CheckLyrics ()
 {
+    var         oneLine;
+
     // alert("Process_CheckLyrics() called");
     songName = document.getElementById("songName").value;
     outStr1  = 'Song name = ' + songName;
@@ -97,7 +133,8 @@ function Process_CheckLyrics ()
         myReader.readAsText(inputFile);
 
         myReader.onload = function() {
-            resultContent    = ''
+            resBody = '';
+            //
             var contentAll   = myReader.result;
             var contentLines = contentAll.split(/\r\n/);
             outStr = 'Number of lines in input file = ' + contentLines.length;
@@ -131,8 +168,6 @@ const myForm = document.querySelector('#inputForm');
 myForm.addEventListener('submit', function(event){
     event.preventDefault();
     Process_CheckLyrics();
-    // console.log('password field is ' + userPwd);
-    // currentHref = window.location.href;
 })
 
 console.log('CheckLyrics.js loaded')
